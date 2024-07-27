a6d5dfdee30c7469902fe7d6f9f44adc21f528ed
unknown <koongclup@gmail.com>
2024-07-27 08:28:25 +0700

    Initial commit

--- inc/config/connection.php
+++ inc/config/connection.php
@@ -2,14 +2,13 @@
 <?php
 $servername = "192.168.212.50";
 $username = "sa";
-$password = "7188455"; //ถ้าไม่ได้ตั้งรหัสผ่านให้ลบ yourpassword ออก
- 
+$password = "7188455"; 
+
 try {
   $conn = new PDO("mysql:host=$servername;dbname=db_employee;charset=utf8", $username, $password);
   $conn->exec("set names utf8");
-  // set the PDO error mode to exception
+
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  //echo "Connected successfully";
 } catch(PDOException $e) {
   echo "Connection failed: " . $e->getMessage();
 }
